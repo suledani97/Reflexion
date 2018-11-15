c230f38d187106b1dd0c4272f7a0bc0c4663eb97
suledani <suledani97@gmail.com>
2018-11-15 13:51:18 +0100

    Changed button, looks better

--- app/src/main/java/hu/bme/aut/reflexion/TestActivity.java
+++ app/src/main/java/hu/bme/aut/reflexion/TestActivity.java
@@ -1,19 +1,11 @@
 package hu.bme.aut.reflexion;
 
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-
-import java.util.Objects;
-import java.util.Random;
-
-import hu.bme.aut.reflexion.fragments.ButtonGreenFragment;
-import hu.bme.aut.reflexion.fragments.ButtonRedFragment;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -21,18 +13,5 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-
-        Button replaceButton = findViewById(R.id.replaceTestButton);
-
-        replaceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.redButtonFragment, ButtonGreenFragment.newInstance());
-                fragmentTransaction.commit();
-            }
-        });
-
     }
 }
