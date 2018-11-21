65e5e18d7d1beed37263010d2b0092dbf990e4fc
suledani <suledani97@gmail.com>
2018-11-21 20:56:38 +0100

    Redesigned Button, Not Fragment anymore Timing works

--- app/src/main/java/hu/bme/aut/reflexion/TestActivity.java
+++ app/src/main/java/hu/bme/aut/reflexion/TestActivity.java
@@ -1,17 +1,33 @@
 package hu.bme.aut.reflexion;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class TestActivity extends AppCompatActivity {
+
+    Button bigButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        bigButton = findViewById(R.id.btnMain);
+
+        //Random idő után a gomb zölddé változik
+        final Handler delayer = new Handler();
+        delayer.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                changeButtonStatus();
+            }
+        }, 3000);
+    }
+
+    private void changeButtonStatus(){
+        bigButton.setBackgroundResource(R.drawable.greenbutton);
+        bigButton.setText(R.string.press_right);
     }
 }
